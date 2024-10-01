ad55fe30f9844e1b7250c10aff19ca26563cd8e1
Estemetek <emaestimada@gmail.com>
2024-10-01 18:21:16 +0800

    altered products and a component

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -5,41 +5,42 @@ import ProductCategoryHeader from './products/ProductCategoryHeader'
 
 const ProductTable = ({headers, products}) => {
 
-    const sportingGoods = products.filter((product) => product.type == 1);
+    const sportinggoods = products.filter((product) => product.type == 1);
     const electronics = products.filter((product) => product.type == 2);
 
-  return(
-    <div>
+    return (
+        <div>
         <Table.Tablecontainer>
-            <Table.THead>
-                <Table.Row>
-                    <Table.ColumnHeader>Name</Table.ColumnHeader>
-                    <Table.ColumnHeader>Price</Table.ColumnHeader>
-                </Table.Row>
-            </Table.THead>
-
-            <Table.TBody>
-                <ProductCategoryHeader text={headers[0]}/>
-
-                {sportingGoods.map((sportingGood) => (
-                    <ProductItem 
-                        key = {`${sportingGood.type}``${sportingGood.id}`}
-                        name = {sportingGood.name}
-                        price = {sportingGood.price}
-                    />
-                ))}
-
-                {electronics.map((sportingGood) => (
-                    <ProductItem 
-                        key = {`${sportingGood.type}``${sportingGood.id}`}
-                        name = {sportingGood.name}
-                        price = {sportingGood.price}
-                    />
-                ))}
-            </Table.TBody>
-        </Table.Tablecontainer>
-    </div>
-  )
-}
-
-export default ProductTable
+          <Table.THead>
+            <Table.Row>
+                <Table.ColumnHeader>Name</Table.ColumnHeader>
+                <Table.ColumnHeader>Price</Table.ColumnHeader>
+            </Table.Row>
+          </Table.THead>
+    
+          <Table.TBody>
+    
+            <ProductCategoryHeader category={headers[0]}/>
+          {sportinggoods.map((sportingGood) => (
+            <ProductItem name={sportingGood.name} price={sportingGood.price}/>
+          ))}
+            
+            <ProductCategoryHeader category={headers[1]}/>
+    
+            {electronics.map(({id,name,price,type}) => (
+              <ProductItem key={`${type}-${id}`} name={name} price={price}/>
+            ))}
+    
+          </Table.TBody>
+          <Table.TFoot>
+            <Table.Row>
+                <Table.ColumnHeader colspan="2">TOTAL</Table.ColumnHeader>
+                <Table.Column>${products.reduce((previousvalue, currentvalue) => previousvalue + currentvalue.price, 0)}</Table.Column>
+            </Table.Row>
+          </Table.TFoot>
+          </Table.Tablecontainer>
+        </div>
+      )
+    }
+    
+    export default ProductTable
--- src/components/products/ProductCategoryHeader.jsx
+++ src/components/products/ProductCategoryHeader.jsx
@@ -1,14 +1,14 @@
-//ProductCategoryHeader.jsx
-
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react'
-import Table from '../Table';
+import Table from '../Table'
 
-const ProductCategoryHeader = ({text}) => {
+const ProductCategoryHeader = ({category}) => {
   return (
     <Table.Row>
-        <Table.ColumnHeader colspan="2">{text}</Table.ColumnHeader>
+        <Table.ColumnHeader colspan="2">{category}</Table.ColumnHeader>
     </Table.Row>
-    );
+  )
 }
 
 export default ProductCategoryHeader
